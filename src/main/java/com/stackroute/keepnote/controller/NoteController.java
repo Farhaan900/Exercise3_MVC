bc5ef9bb8b744a636ae38f343b560180fc8b987e
Farhaan900 <mfarhaan.shaikh95@gmail.com>
2019-03-06 18:17:48 +0530

    commit 1

--- src/main/java/com/stackroute/keepnote/controller/NoteController.java
+++ src/main/java/com/stackroute/keepnote/controller/NoteController.java
@@ -1,11 +1,32 @@
 package com.stackroute.keepnote.controller;
 
 
-/*Annotate the class with @Controller annotation. @Controller annotation is used to mark 
+import com.stackroute.keepnote.model.Note;
+import com.stackroute.keepnote.repository.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
+
+/*Annotate the class with @Controller annotation. @Controller annotation is used to mark
  * any POJO class as a controller so that Spring can recognize this class as a Controller
  * */
-
+@RequestMapping("")
+@Controller
 public class NoteController {
+	@Autowired
+	ApplicationContext applicationContextExt;
+	Note noteExt;
+	NoteRepository noteRepositoryExt;
+
 	/*
 	 * From the problem statement, we can understand that the application
 	 * requires us to implement the following functionalities.
@@ -16,18 +37,31 @@ public class NoteController {
 	 * 3. Delete an existing note.
 	 * 4. Update an existing note.
 	 */
-	
+
+
+
+
 	/* 
 	 * Get the application context from resources/beans.xml file using ClassPathXmlApplicationContext() class.
 	 * Retrieve the Note object from the context.
 	 * Retrieve the NoteRepository object from the context.
 	 */
-	
+
+	public NoteController(){
+		applicationContextExt = new ClassPathXmlApplicationContext("beans.xml");
+		noteExt = (Note)applicationContextExt.getBean("note");
+		noteRepositoryExt = (NoteRepository) applicationContextExt.getBean("noteRepository");
+	}
 	
 	/*Define a handler method to read the existing notes by calling the getAllNotes() method 
 	 * of the NoteRepository class and add it to the ModelMap which is an implementation of Map 
 	 * for use when building model data for use with views. it should map to the default URL i.e. "/" */
-	
+
+	@RequestMapping("/*")
+	public String printHello(ModelMap map){
+		map.addAttribute("users",noteRepositoryExt.getAllNotes());
+		return "index";
+	}
 	
 	/*Define a handler method which will read the Note data from request parameters and
 	 * save the note by calling the addNote() method of NoteRepository class. Please note 
@@ -37,11 +71,38 @@ public class NoteController {
 	 * should be sent back to the view using ModelMap.
 	 * This handler method should map to the URL "/saveNote". 
 	*/
-	
+	@RequestMapping("/saveNote")
+	public String display(ModelMap map, @RequestParam("noteId") int  noteId,
+						  @RequestParam("noteContent") String  noteContent,
+						  @RequestParam("noteTitle") String  noteTitle,
+						  @RequestParam("noteStatus") String noteStatus)
+	{
+		noteExt = new Note();
+		noteExt.setNoteId(noteId);
+		noteExt.setNoteContent(noteContent);
+		noteExt.setNoteTitle(noteTitle);
+		noteExt.setNoteStatus(noteStatus);
+		noteExt.setCreatedAt(LocalDateTime.now());
+		noteRepositoryExt.addNote(noteExt);
+		map.addAttribute("users",noteRepositoryExt.getAllNotes());
+		return "index";
+	}
+
 	
 	/* Define a handler method to delete an existing note by calling the deleteNote() method 
 	 * of the NoteRepository class
 	 * This handler method should map to the URL "/deleteNote" 
 	*/
+
+	@RequestMapping("/deleteNote")
+	public String deleteNote(HttpServletRequest request, HttpServletResponse response) {
+
+		noteRepositoryExt.deleteNote(Integer.parseInt(request.getParameter("noteId")));
+		return "redirect:/";
+
+	}
+
+
+
 	
 }
--- src/main/java/com/stackroute/keepnote/model/Note.java
+++ src/main/java/com/stackroute/keepnote/model/Note.java
@@ -7,6 +7,13 @@ import java.time.LocalDateTime;
  */
 public class Note {
 
+	private int noteId;
+	private String noteTitle;
+	private String noteContent;
+	private String noteStatus;
+	private LocalDateTime createdAt;
+
+
 	/*
 	 * This class should have five fields (noteId, noteTitle, noteContent,
 	 * noteStatus and createdAt). This class should also contain the getters and
@@ -20,50 +27,58 @@ public class Note {
 	/* All the getters/setters definition should be implemented here */
 
 	public int getNoteId() {
-		return 0;
+		return noteId;
 
 	}
 
 	public void setNoteId(int intid) {
+		this.noteId = intid;
 
 	}
 
 	public String getNoteTitle() {
-		return null;
+
+		return noteTitle;
 	}
 
 	public void setNoteTitle(String string) {
-
+		this.noteTitle = string;
 	}
 
 	public String getNoteContent() {
-		return null;
+		return noteContent;
 	}
 
 	public void setNoteContent(String string) {
-
+		this.noteContent = string;
 	}
 
 	public String getNoteStatus() {
-		return null;
+		return noteStatus;
 	}
 
 	public void setNoteStatus(String string) {
-
+		this.noteStatus = string;
 	}
 
 	public LocalDateTime getCreatedAt() {
-		return null;
+		return createdAt;
 	}
 
 	public void setCreatedAt(LocalDateTime localdatetime) {
-
+		this.createdAt = localdatetime;
 	}
 
 	/* Override the toString() method */
 
 	@Override
 	public String toString() {
-		return null;
+		return "Note{" +
+				"noteId=" + noteId +
+				", noteTitle='" + noteTitle + '\'' +
+				", noteContent='" + noteContent + '\'' +
+				", noteStatus='" + noteStatus + '\'' +
+				", createdAt=" + createdAt +
+				'}';
 	}
 }
